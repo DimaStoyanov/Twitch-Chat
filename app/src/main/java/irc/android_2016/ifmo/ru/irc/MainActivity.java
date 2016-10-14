24669764bb1db590622f35c1ee7e050a40622e9a
Dima <sdima11101@gmail.com>
2016-10-15 01:11:46 +0300

    Initial commit

---
+++ app/src/main/java/irc/android_2016/ifmo/ru/irc/MainActivity.java
@@ -0,0 +1,13 @@
+package irc.android_2016.ifmo.ru.irc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
